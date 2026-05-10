2854b5e787c840318179bc84c763ef9e6596e97a
Dev Sharma <dev47929@gmail.com>
2026-05-10 19:41:21 +0530

    Empty Branch

--- frontendv2/src/constants.js
+++
@@ -1,8 +0,0 @@
-export const CATEGORIES = [
-  { id: 'water', name: 'Water Supply', icon: 'Droplets', color: 'blue' },
-  { id: 'electricity', name: 'Electricity', icon: 'Zap', color: 'yellow' },
-  { id: 'roads', name: 'Roads & Potholes', icon: 'Road', color: 'orange' },
-  { id: 'sanitation', name: 'Sanitation', icon: 'Trash2', color: 'green' },
-  { id: 'safety', name: 'Public Safety', icon: 'Shield', color: 'red' },
-  { id: 'others', name: 'Others', icon: 'MoreHorizontal', color: 'gray' },
-];
